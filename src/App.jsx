3b6083f31008803b08f68cd6e0e242d6847c5299
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-06-04 14:44:13 +0200

    compare this week with last week

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import population from './population';
 
 function App() {
   const [heading, setHeading] = useState('heading');
@@ -17,6 +18,9 @@ function App() {
     setCells(cells);
   }, []);
 
+  const week = cells.slice(cells.length - 7);
+  const prev = cells.slice(cells.length - 14, cells.length - 7);
+
   return (
     <div>
       <div className="table">
@@ -24,13 +28,51 @@ function App() {
         {columns.map((column) => (
           <span className="row-heading">{column.replace(/_/g, ' ')}</span>
         ))}
-        {rows.slice(rows.length - 7).map((row, j) => (
+        {rows.slice(rows.length - 14).map((row, j) => (
           <>
-            <span className="column-heading">{rows[rows.length - j - 1]}</span>
+            <span className="column-heading">
+              {rows[rows.length - j - 1].substr(5)}
+            </span>
             {columns.map((column, i) => (
               <span className="cell">{cells[rows.length - j - 1]?.[i]}</span>
             ))}
           </>
+        ))}
+
+        <span className="column-heading">∑ vecka</span>
+        {columns.map((column, i) => (
+          <span className="cell">
+            {week.map((cells) => cells?.[i]).reduce((a, b) => a + b, 0)}
+          </span>
+        ))}
+
+        <span className="column-heading">∑ föreg v</span>
+        {columns.map((column, i) => (
+          <span className="cell">
+            {prev.map((cells) => cells?.[i]).reduce((a, b) => a + b, 0)}
+          </span>
+        ))}
+
+        <span className="column-heading">vecka/dag</span>
+        {columns.map((column, i) => (
+          <span className="cell">
+            {Math.round(
+              week.map((cells) => cells?.[i]).reduce((a, b) => a + b, 0) /
+                7e-6 /
+                population[column],
+            )}
+          </span>
+        ))}
+
+        <span className="column-heading">föreg/dag</span>
+        {columns.map((column, i) => (
+          <span className="cell">
+            {Math.round(
+              prev.map((cells) => cells?.[i]).reduce((a, b) => a + b, 0) /
+                7e-6 /
+                population[column],
+            )}
+          </span>
         ))}
       </div>
     </div>

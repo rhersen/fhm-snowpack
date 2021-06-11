5ffd1b0b8c5dec1ef516e734d3a194d1649cb48f
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-06-11 15:50:20 +0200

    recoil with selectors

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import population from './population';
-import { atom, useRecoilState, useRecoilValue } from 'recoil';
+import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 
-const headingState = atom({ key: 'headingState', default: 'heading' });
-const columnState = atom({ key: 'columnState', default: [] });
-const cellState = atom({ key: 'cellState', default: [] });
+const dataState = atom({ key: 'dataState', default: {} });
+const headingState = selector({ key: 'headingState', get: ({ get }) => get(dataState).heading });
+const columnState = selector({ key: 'columnState', get: ({ get }) => get(dataState).columns });
+const cellState = selector({ key: 'cellState', get: ({ get }) => get(dataState).cells });
 
 function App() {
-  const [heading, setHeading] = useRecoilState(headingState);
-  const [columns, setColumns] = useRecoilState(columnState);
-  const [cells, setCells] = useRecoilState(cellState);
+  const [, setData] = useRecoilState(dataState);
 
   useEffect(async () => {
     const response = await fetch('https://secure.hersen.name/fohm');
     const data = await response.json();
-    const { heading, columns, cells } = data;
-    setHeading(heading);
-    setColumns(columns);
-    setCells(cells);
+    setData(data);
   }, []);
 
   const week = useRecoilValue(cellState).slice(
